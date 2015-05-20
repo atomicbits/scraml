14ef4f7370d4b85db886f604476eea0d3122a344
Peter Rigole <peter.rigole@atomicbits.io>
2015-05-20 16:36:49 +0200

    Fixing dependency issues to the java-raml-parser 0.9-SNAPSHOT.

--- project/ApplicationBuild.scala
+++ project/ApplicationBuild.scala
@@ -10,22 +10,27 @@ with Dependencies {
     id = "scramlgen-parser",
     base = file("modules/scramlgen-parser"),
     settings = projectSettings(dependencies = scramlgenParserDeps ++ testDeps)
-  ) settings (resolvers += "Local Maven Repository" at "file:///Users/peter/.m2/repository")
+  ) settings (
+    // Sonatype snapshot resolver is needed to fetch raml-java-parser 0.9-SNAPSHOT.
+    resolvers += "Sonatype OSS Snapshots" at "https://oss.sonatype.org/content/repositories/snapshots"
+    )
 
   val scramlgen = Project(
     id = "scramlgen",
     base = file("modules/scramlgen"),
     settings = projectSettings(dependencies = scramlgenDeps ++ testDeps)
-  ) settings(
-    resolvers += "Local Maven Repository" at "file:///Users/peter/.m2/repository" // why does Resolver.mavenLocal not work?
+  ) settings (
+    // Sonatype snapshot resolver is needed to fetch raml-java-parser 0.9-SNAPSHOT.
+    resolvers += "Sonatype OSS Snapshots" at "https://oss.sonatype.org/content/repositories/snapshots"
     ) dependsOn scramlgenParser
 
   val scramlgenTest = Project(
     id = "scramlgen-test",
     base = file("modules/scramlgen-test"),
     settings = projectSettings(dependencies = scramlgenTestDeps ++ testDeps)
-  ) settings(
-    resolvers += "Local Maven Repository" at "file:///Users/peter/.m2/repository"
+  ) settings (
+    // Sonatype snapshot resolver is needed to fetch raml-java-parser 0.9-SNAPSHOT.
+    resolvers += "Sonatype OSS Snapshots" at "https://oss.sonatype.org/content/repositories/snapshots"
     )
 
   val scramlgenTestDef = Project(
@@ -33,7 +38,8 @@ with Dependencies {
     base = file("modules/scramlgen-testdef"),
     settings = projectSettings(dependencies = scramlgenDeps ++ testDeps)
   ) settings(
-    resolvers += "Local Maven Repository" at "file:///Users/peter/.m2/repository",
+    // Sonatype snapshot resolver is needed to fetch raml-java-parser 0.9-SNAPSHOT.
+    resolvers += "Sonatype OSS Snapshots" at "https://oss.sonatype.org/content/repositories/snapshots",
     addCompilerPlugin("org.scalamacros" % "paradise" % "2.1.0-M5" cross CrossVersion.full),
     incOptions := incOptions.value.withNameHashing(false) // See issue: https://github.com/sbt/sbt/issues/1593
     ) dependsOn scramlgen
