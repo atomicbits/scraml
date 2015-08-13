7334df045375b8488950c29f23500085d485c113
Peter Rigole <peter.rigole@atomicbits.io>
2015-08-13 13:23:15 +0200

    Testing publish settings.

--- project/BuildSettings.scala
+++ project/BuildSettings.scala
@@ -42,7 +42,7 @@ trait BuildSettings {
     scalacOptions := scalacBuildOptions,
     parallelExecution := false,
     // Sonatype snapshot resolver is needed to fetch rxhttpclient-scala_2.11:0.2.0-SNAPSHOT.
-    resolvers += "Sonatype OSS Snapshots" at "https://oss.sonatype.org/content/repositories/snapshots",
+//    resolvers += "Sonatype OSS Snapshots" at "https://oss.sonatype.org/content/repositories/snapshots",
     libraryDependencies ++= extraDependencies
   )
 
---
+++ project/plugins.sbt
@@ -0,0 +1,3 @@
+// supports release in maven central
+addSbtPlugin("org.xerial.sbt" % "sbt-sonatype" % "0.5.1")
+addSbtPlugin("com.jsuereth" % "sbt-pgp" % "1.0.0")
