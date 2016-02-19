1c282b087c411efd62e54d5295c0f0ceb5f3dd5e
Peter Rigole <peter.rigole@atomicbits.io>
2016-02-19 13:51:35 +0100

    Implemented: Do not require an id on inline schema definitions #25 We're ready for release 0.5.0.

--- project/BuildSettings.scala
+++ project/BuildSettings.scala
@@ -63,7 +63,7 @@ trait BuildSettings {
     publishTo := {
       val nexus = "https://oss.sonatype.org/"
       if (isSnapshot.value)
-        Some("snapshots" at nexus + "content/repositories/snapshots")
+        None // Some("snapshots" at nexus + "content/repositories/snapshots")
       else
         Some("releases" at nexus + "service/local/staging/deploy/maven2")
     },
