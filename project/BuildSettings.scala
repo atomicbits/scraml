14824d5d46337747b2d08262bd92ea7cf2024d02
Peter Rigole <peter.rigole@atomicbits.io>
2017-06-18 18:22:03 +0200

    Version bump to 0.7.0-M2-SNAPSHOT.

--- project/BuildSettings.scala
+++ project/BuildSettings.scala
@@ -25,7 +25,7 @@ object BuildSettings {
 
   val snapshotSuffix = "-SNAPSHOT"
 
-  val Version = "0.7.0-M1" // + snapshotSuffix
+  val Version = "0.7.0-M2" + snapshotSuffix
 
   val scala2_10 = "2.10.5"
   val scala2_11 = "2.11.8"
