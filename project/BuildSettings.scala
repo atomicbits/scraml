f55ff34f2a91943e7874afaa0ca605a14d132c1b
Peter Rigole <peter.rigole@atomicbits.io>
2017-10-11 13:32:44 +0200

    Version bump to 0.8.0-SNAPSHOT.

--- project/BuildSettings.scala
+++ project/BuildSettings.scala
@@ -25,7 +25,7 @@ object BuildSettings {
 
   val snapshotSuffix = "-SNAPSHOT"
 
-  val Version = "0.7.0" // + snapshotSuffix // Change in 1 place
+  val Version = "0.8.0" + snapshotSuffix // Change in 1 place
 
   val scala2_10 = "2.10.6"
   val scala2_11 = "2.11.11"
